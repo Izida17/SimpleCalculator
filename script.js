8a4197ced9bde00e0a32a1a74642ae7b8bca86ae
Izida17 <arina.zipr2325@gmail.com>
2025-03-09 23:34:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let display = document.getElementById('display'); // Получаем ссылку на поле ввода
+let historyDiv = document.getElementById('history'); // Получаем ссылку на div истории
+let currentExpression = ""; // переменная для хранения текущего выражения
+
+function appendToDisplay(value) {
+    currentExpression += value;  // Добавляем значение к текущему выражению
+    display.value = currentExpression; // Обновляем отображение
+}
+
+function clearDisplay() {
+    currentExpression = ""; // Очищаем текущее выражение
+    display.value = ""; // Очищаем дисплей
+}
+
+function calculate() {
+    try {
+        let result = eval(currentExpression); // Вычисляем результат
+        display.value = result; // Отображаем результат
+
+        addToHistory(currentExpression + " = " + result); // Добавляем в историю
+        currentExpression = result.toString(); //Сохраняем результат для след вычислений
+
+    } catch (error) {
+        display.value = "Error"; // Отображаем ошибку
+    }
+}
+
+function addToHistory(expression) {
+    let newEntry = document.createElement('p'); // Создаем новый элемент <p>
+    newEntry.textContent = expression; // Устанавливаем текст элемента
+    historyDiv.appendChild(newEntry); // Добавляем элемент в блок истории
+}
